e843928d042b73059239956fc435860c5277fd55
matusiakk <35163600+matusiakk@users.noreply.github.com>
2018-04-29 11:04:26 +0200

    poprawki w formularzu
    poprawki w przekazywaniu danych do bazy
    dodanie alertów przy niewypełnionych polach

--- Motivate Me - Kasia/www/js/forms.js
+++ Motivate Me - Kasia/www/js/forms.js
@@ -1,17 +1,48 @@
 //zapisywanie celu do bazy
 
-var rootRef = firebase.database().ref().child('goal'+ Math.floor((Math.random() * 100000)));
-
 $('#save').click(
-function(){
-	rootRef.set({
-		user: "dL2lNHV9OYa8PUVCkRaSNqEkcHd2",
-		cel: $('#cel').val(),
-		opis : $('#opis').val(),
-		startDate : new Date(),
-		endDate : $('#endDate').val(),
-		zgoda : $('#agree').is(':checked')
-	});
-})
 
-  
+function saveGoal(){
+	
+var user = "dL2lNHV9OYa8PUVCkRaSNqEkcHd2";
+var cel = $('#cel').val();
+var opis = $('#opis').val();
+
+function zero(liczba) {
+    return liczba=(liczba < 10)? "0"+liczba : liczba;
+}
+var today = new Date();
+var start = today.getFullYear()+'-'+(zero(today.getMonth()+1))+'-'+zero(today.getDate());
+
+var end = $('#endDate').val();
+var zgoda =  $('#agree').is(':checked');
+var name = "goal-" + user +"-"+ cel;
+var rootRef = firebase.database().ref().child(name);
+
+
+	if (cel == ""){
+	alert ("Cel field cannot be blank");
+	}
+	
+	else if (opis == ""){
+	alert ("Opis field cannot be blank");
+	}
+	
+	else if (end == ""){
+	alert ("Date field cannot be blank");
+	}
+	
+	else {
+	rootRef.set({
+		user: user,
+		cel: cel,
+		opis : opis,
+		startDate : start,
+		endDate : end,
+		zgoda : zgoda
+	})
+	alert ("the goal has been saved");
+	}
+console.log(name +"\n"+user+"\n"+cel+"\n"+opis+"\n"+start+"\n"+end+"\n"+zgoda);
+	}
+)

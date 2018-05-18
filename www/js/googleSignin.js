f009036c40c2c81bb57a468a0ef22ebbdbf84f07
Magdalena Gorka <mkgorka93@gmail.com>
2018-05-18 22:33:23 +0200

    update

--- www/js/googleSignin.js
+++ www/js/googleSignin.js
@@ -1,15 +1,21 @@
 $('#google').click(
 
-  function () {
-    if (!firebase.auth().currentUser) {
-      let provider = new firebase.auth.GoogleAuthProvider();
+    function () {
+        if (window.plugins) {
+            window.plugins.googleplus.login(
+                {
+                    'webClientId': '1064101044778-antang1guujbeobg08ocrk7cqq01rb1s.apps.googleusercontent.com',
+                    'offline': true
+                },
+                function (obj) {
+                    if (!firebase.auth().currentUser) {
+                        firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(obj.idToken)).then(function (succes) {
 
-      firebase.auth().signInWithRedirect(provider);
+                        }).catch(function (error) {
 
-    } else {
-
-      firebase.auth().signOut();
-
-    }
-
-  });
+                        });
+                    } else {
+                    }
+                })
+        }
+    })
